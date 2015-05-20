bfc7e08ba70ba0e3acb9e4cc69b70c816845b6cb
Dominic Rodger <dominicrodger+github@gmail.com>
2015-05-20 08:27:46 +0100

    Include uncategorised spending in overview pie chart
    Also, only show last 120 days

--- djofx/views/home.py
+++ djofx/views/home.py
@@ -1,8 +1,10 @@
+from datetime import date, timedelta
 from django.db.models import Sum
 from django.views.generic import TemplateView
 from djofx.forms import OFXForm
 from djofx.views.base import PageTitleMixin, UserRequiredMixin
 from djofx import models
+from operator import itemgetter
 
 
 class HomePageView(PageTitleMixin, UserRequiredMixin, TemplateView):
@@ -16,15 +18,27 @@ class HomePageView(PageTitleMixin, UserRequiredMixin, TemplateView):
 
         context['form'] = OFXForm()
 
+        cutoff = date.today() - timedelta(days=120)
+
+        uncategorised_breakdown = models.Transaction.objects.filter(
+            amount__lt=0,
+            transaction_category__isnull=True,
+            date__gte=cutoff
+        ).aggregate(
+            total=Sum('amount')
+        )
+
         breakdown = models.Transaction.objects.filter(
             amount__lt=0,
-            transaction_category__is_void=False
+            transaction_category__is_void=False,
+            date__gte=cutoff
         ).values(
             'transaction_category__pk',
             'transaction_category__name'
         ).annotate(
             total=Sum('amount')
         ).order_by('-total')
+
         context['breakdown'] = [
             (
                 abs(item['total']),
@@ -33,5 +47,15 @@ class HomePageView(PageTitleMixin, UserRequiredMixin, TemplateView):
             )
             for item in breakdown
         ]
+        context['breakdown'].append(
+            (
+                uncategorised_breakdown['total'] * -1,
+                0,
+                'Uncategorised'
+            )
+        )
+        context['breakdown'] = sorted(context['breakdown'],
+                                      key=itemgetter(0),
+                                      reverse=True)
 
         return context
